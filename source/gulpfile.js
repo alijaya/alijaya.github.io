f63de45e1321776ceda1f3075ea3fa5b372131f2
Ali Jaya Meilio <alijayameilio@gmail.com>
2018-08-06 21:32:18 +0200

    add automation with gulp

---
+++ source/gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const debug = require('gulp-debug');
+const shell = require('gulp-shell');
+const runSequence = require('run-sequence');
+const del = require('del');
+
+gulp.task('clean', function() {
+  return del([
+    '../css',
+    '../js',
+    '../fonts',
+    '../favicon.ico',
+    '../index.html'
+  ], {
+    force: true // danger zone, delete outside folders
+  })
+})
+
+gulp.task('build', shell.task('vue-cli-service build'));
+
+gulp.task('copy', function() {
+  return gulp.src('dist/**/*')
+    .pipe(gulp.dest('../'))
+});
+
+gulp.task('deploy', function(cb) {
+  runSequence('clean', 'build', 'copy', cb);
+})
